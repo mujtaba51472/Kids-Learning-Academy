12eb4cc28f1adc0b7bdd8d72f001a8baa8084fae
mujtaba <mujtaba51472@gmail.com>
2021-01-20 22:46:46 +0500

    first commit

---
+++ ex.js
@@ -0,0 +1,9 @@
+
+var month= new Array( "jan","feb" , "march", "april" , "may ", "june" ,"july");
+var day=["Foo" ,"app" , "Boo"];
+var num=[ 12,16,15, 20.1,22,2.2, 30,3,4,40, 42];
+
+console.log(...month.copyWithin(4,6));
+
+var z=4;
+console.log("z= "+z);
